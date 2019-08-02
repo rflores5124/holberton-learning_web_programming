9e50cb22fa5b62154d89d9b4c392267b1e9b18f6
rflores5124 <rflores5124@gmail.com>
2019-08-02 14:13:30 -0400

    Update 3-my_first_loop.js

--- 5-javascript/3-my_first_loop.js
+++ 5-javascript/3-my_first_loop.js
@@ -1,3 +1,23 @@
+console.log("Hello world")
+5+5
+10
+6-6
+0
+3*4
+12
+20/2
+10
+
+let x = 2
+undefined
+console.log(x)
+VM622:1 2
+undefined
+let variable_test ="Holberton"
+undefined
+console.log(variable_test)
+VM779:1 Holberton
+undefined
 0
 1
 2
