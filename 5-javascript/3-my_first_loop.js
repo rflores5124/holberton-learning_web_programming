ada0ffe1ad150c1af15383282b62f9e10e967545
rflores5124 <rflores5124@gmail.com>
2019-08-02 14:45:05 -0400

    Update 3-my_first_loop.js

--- 5-javascript/3-my_first_loop.js
+++ 5-javascript/3-my_first_loop.js
@@ -1,31 +1,6 @@
-console.log("Hello world")
-5+5
-10
-6-6
-0
-3*4
-12
-20/2
-10
+var i = 0;
+while (i<=10) {
+  console.log(i);
+  i++;
 
-let x = 2
-undefined
-console.log(x)
-VM622:1 2
-undefined
-let variable_test ="Holberton"
-undefined
-console.log(variable_test)
-VM779:1 Holberton
-undefined
-0
-1
-2
-3
-4
-5
-6
-7
-8
-9
-10
+}
