adf7782518ab077ac63b174557313e8345ac7b74
rflores5124 <rflores5124@gmail.com>
2019-08-02 14:05:19 -0400

    Update 0-hellow_world.js

--- 5-javascript/0-hellow_world.js
+++ 5-javascript/0-hellow_world.js
@@ -1,5 +1,4 @@
 console.log("Hello world")
-
 5+5
 10
 6-6
@@ -8,3 +7,14 @@ console.log("Hello world")
 12
 20/2
 10
+
+let x = 2
+undefined
+console.log(x)
+VM622:1 2
+undefined
+let variable_test ="Holberton"
+undefined
+console.log(variable_test)
+VM779:1 Holberton
+undefined
